0c00db0c89a0c9882390246c2266f42cda426712
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-06-25 19:48:29 +0200

    adding auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth";
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import NextAuth from 'next-auth'
+import prisma from './lib/prisma'
+import GitHub from 'next-auth/providers/github'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-});
+    adapter: PrismaAdapter(prisma),
+    providers: [GitHub],
+})
